a8e148ec7a33e5ef376d0e82c5e01ffafa55db44
Heitor Freitas <heitor.ff@ufu.br>
2020-05-22 01:52:55 -0300

    Finalizada API com todos os metodos REST

--- wiki-api/app.js
+++ wiki-api/app.js
@@ -27,104 +27,212 @@ const articleSchema = new Schema({
 const Article = mongoose.model("Article", articleSchema)
 const Gitdoc = mongoose.model("Gitdoc", articleSchema)
 
+///////////////////////////////////////////////////
+app.route("/:route")
+  .get((req, res) => {
+    const route = req.params.route
+    // console.log(route);
+    if (route === "articles") {
+      Article.find({}, (err, foundArticles) => {
+        if (!err) {
+          res.send(foundArticles)
+        } else {
+          res.send(err)
+        }
+      })
+    }
+    if (route === "git") {
+      Gitdoc.find({}, (err, foundArticles) => {
+        if (!err) {
+          res.send(foundArticles)
+        } else {
+          res.send(err)
+        }
+      })
+    }
+  })
+  .post((req, res) => {
+    const route = req.params.route
+    console.log(req.body.title)
+    console.log(req.body.content)
+    // console.log(route);
+    if (route === "articles") {
+      const newArticle = new Article({
+        title: req.body.title,
+        content: req.body.content
+      });
+      newArticle.save((err) => {
+        if (!err) {
+          res.send("Artigo adicionado com sucesso")
+        } else {
+          res.send(err)
+        }
+      })
+    }
+    if (route === "git") {
+      const newGitdoc = new Gitdoc({
+        title: req.body.title,
+        content: req.body.content
+      });
+      newGitdoc.save((err) => {
+        if (!err) {
+          res.send("Documentação sobre Git adicionada com sucesso")
+        } else {
+          res.send(err)
+        }
+      })
+    }
+  })
+  .delete((req, res) => {
+    const route = req.params.route
+    // console.log(req.body.title)
+    // console.log(req.body.content)
+    // console.log(route);
+    if (route === "articles") {
+      Article.deleteMany({}, (err) => {
+        if (!err) {
+          res.send("Artigos deletados com sucesso")
+        } else {
+          res.send(err)
+        }
+      })
+    } else if (collection === "git") {
+      Gitdoc.deleteMany({}, (err) => {
+        if (!err) {
+          res.send("Artigos deletados com sucesso")
+        } else {
+          res.send(err)
+        }
+      })
+    }
+  })
 
-//app.route("/:route").get().post().delete()
+/////////////////////////////////////////////////////
+app.route('/:collection/:documentTitle')
+  .get((req, res) => {
+    const collection = req.params.collection
+    const documentTitle = req.params.documentTitle
+    if (collection === "articles") {
+      Article.findOne({
+        title: documentTitle
+      }, (err, foundArticle) => {
+        if (foundArticle) {
+          res.send(foundArticle)
+        } else {
+          res.send("Não há artigos com esse titulo")
+        }
+      })
+    } else if (collection === "git") {
+      Gitdoc.findOne({
+        title: documentTitle
+      }, (err, foundArticle) => {
+        if (foundArticle) {
+          res.send(foundArticle)
+        } else {
+          res.send("Não há nada na documentação de git com esse titulo")
+        }
+      })
+    }
+  })
+  .put((req, res) => {
+    const collection = req.params.collection
+    const documentTitle = req.params.documentTitle
+    if (collection === "articles") {
+      Article.replaceOne({
+        title: req.params.documentTitle
+      }, {
+        title: req.body.title,
+        content: req.body.content
+      }, {
+        overwrite: true
+      }, function (err) {
+        if (!err) {
+          res.send(req.params.documentTitle + " atualizado com sucesso")
+        } else {
+          res.send(err)
+        }
+      });
+    } else if (collection === "git") {
+      Gitdoc.replaceOne({
+        title: req.params.documentTitle
+      }, {
+        title: req.body.title,
+        content: req.body.content
+      }, {
+        overwrite: true
+      }, function (err) {
+        if (!err) {
+          res.send(req.params.documentTitle + " atualizado com sucesso")
+        } else {
+          res.send(err)
+        }
+      });
+    }
+    // else if (collection === "git") {
 
-
-app.get("/:route", (req, res) => {
-  const route = req.params.route
-  // console.log(route);
-  if (route === "articles") {
-    Article.find({}, (err, foundArticles) => {
-      if (!err) {
-        res.send(foundArticles)
-      } else {
-        res.send(err)
-      }
-    })
-  }
-  if (route === "git") {
-    Gitdoc.find({}, (err, foundArticles) => {
-      if (!err) {
-        res.send(foundArticles)
-      } else {
-        res.send(err)
-      }
-    })
-  }
-});
-app.post("/:route", (req, res) => {
-  const route = req.params.route
-  console.log(req.body.title)
-  console.log(req.body.content)
-  // console.log(route);
-  if (route === "articles") {
-    const newArticle = new Article({
-      title: req.body.title,
-      content: req.body.content
-    });
-    newArticle.save((err) => {
-      if (!err) {
-        res.send("Artigo adicionado com sucesso")
-      } else {
-        res.send(err)
-      }
-    })
-  }
-  if (route === "git") {
-    const newGitdoc = new Gitdoc({
-      title: req.body.title,
-      content: req.body.content
-    });
-    newGitdoc.save((err) => {
-      if (!err) {
-        res.send("Documentação sobre Git adicionada com sucesso")
-      } else {
-        res.send(err)
-      }
-    })
-  }
-});
-
-app.delete("/:route", (req, res) => {
-  const route = req.params.route
-  // console.log(req.body.title)
-  // console.log(req.body.content)
-  // console.log(route);
-  if (route === "articles") {
-    // const newArticle = new Article({
-    //   title: req.body.title,
-    //   content: req.body.content
-    // });
-    // newArticle.save((err) => {
-    //   if (!err) {
-    //     res.send("Artigo adicionado com sucesso")
-    //   } else {
-    //     res.send(err)
-    //   }
-    // })
-    Article.deleteMany({}, (err) => {
-      if (!err) {
-        res.send("Artigos deletados com sucesso")
-      } else {
-        res.send(err)
-      }
-    })
-  }
-  if (route === "git") {
-    // const newGitdoc = new Gitdoc({
-    //   title: req.body.title,
-    //   content: req.body.content
-    // });
-    // newGitdoc.save((err) => {
-    //   if (!err) {
-    //     res.send("Documentação sobre Git adicionada com sucesso")
-    //   } else {
-    //     res.send(err)
-    //   }
-    // })
-  }
-});
+    // }
+  })
+  .patch((req, res) => {
+    const collection = req.params.collection
+    const documentTitle = req.params.documentTitle
+    const bodyTitle = req.body.title
+    const bodyContent = req.body.content
+    if (collection === "articles") {
+      Article.update({
+        title: documentTitle
+      }, {
+        $set: req.body
+      }, (err) => {
+        if (!err) {
+          res.send("PATCH em " + collection + " feito com sucesso");
+        } else {
+          res.send(err)
+        }
+      })
+    } else if (collection === "git") {
+      Gitdoc.update({
+        title: documentTitle
+      }, {
+        $set: req.body
+      }, (err) => {
+        if (!err) {
+          res.send("PATCH em " + collection + " feito com sucesso");
+        } else {
+          res.send(err)
+        }
+      })
+    }
+  })
+  .delete((req, res) => {
+    const collection = req.params.collection
+    const documentTitle = req.params.documentTitle
+    const bodyTitle = req.body.title
+    const bodyContent = req.body.content
+    if (collection === "articles") {
+      Article.deleteOne({
+        title: documentTitle
+      }, (err) => {
+        if (!err) {
+          res.send(documentTitle + " deletado com sucesso")
+        } else {
+          res.send(err)
+        }
+      })
+    } else if (collection === "git") {
+      Git.deleteOne({
+        title: documentTitle
+      }, (err) => {
+        if (!err) {
+          res.send(documentTitle + " deletado com sucesso")
+        } else {
+          res.send(err)
+        }
+      })
+    }
+  })
+// app.get("/:route", );
+// app.post("/:route", );
+// app.delete("/:route", );
 app.listen(port, function () {
   console.log("Servidor rodando na porta " + port);
 })
