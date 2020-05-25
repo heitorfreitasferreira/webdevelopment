016713c032a0cdc85ecccb26f635b51c3fa28d0f
Heitor Freitas <heitor.ff@ufu.br>
2020-05-25 10:31:21 -0300

    Iniciado projeto de autenticação, arquivos iniciais e configs feitas

---
+++ Secrets/app.js
@@ -0,0 +1,74 @@
+//jshint esversion:6
+const express = require('express')
+const _ = require('lodash')
+const bodyParser = require('body-parser')
+const ejs = require('ejs')
+const mongoose = require('mongoose')
+
+const port = 3000
+
+const app = express()
+
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+app.set('view engine', 'ejs')
+
+
+mongoose.connect('mongodb://localhost:27017/userDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+const Schema = mongoose.Schema
+
+const userSchemma = new Schema({
+  email: {
+    type: String,
+    required: [true, 'Insira o email']
+  },
+  password: {
+    type: String,
+    required: [true, 'Insira a senha']
+  }
+})
+
+const User = new mongoose.model('User', userSchemma)
+
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+app.get('/login', (req, res) => {
+  res.render('login')
+})
+app.get('/register', (req, res) => {
+  res.render('register')
+})
+
+app.post('/register', (req, res) => {
+  const email = req.body.username
+  const password = req.body.password
+  const newUser = new User({
+    email: email,
+    password: password
+  })
+  newUser.save((err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("secret")
+    }
+  })
+})
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+  console.log("Server up and running on port " + port);
+})
